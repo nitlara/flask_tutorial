fc1e00e29f62a1c611f19205ebbe5cc577e5d072
nitlara <nitlara@gmail.com>
2021-02-24 16:05:50 +0000

    flask api tutorial

---
+++ src/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify, url_for, make_response
+import json
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_body=json.loads(request_body)
+    todos.append(decoded_body)
+    print("Incoming request with the following body", request_body)
+    return jsonify(todos)
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    del todos[position]
+    return jsonify(todos)
+
+
+# These two lines should always be at the end of your app.py file.
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
